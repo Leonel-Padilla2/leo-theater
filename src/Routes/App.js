17c78a33e42ef9d857f713be226eaf77ced28b5c
Leonel-Padilla2 <axcelpadilla17@gmail.com>
2023-01-12 01:14:18 -0600

    feat: adding main routes.

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -1,12 +1,17 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import NavBar from '../Components/NavBar/NavBar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className="app-container">
       <HashRouter>
+        <NavBar/>
+        
         <Routes>
-          <Route path='/' element={<h1>home</h1>}/>
+          <Route path='/' element={<h1>HOME</h1>}/>
+          <Route path='/movies' element={<h1>MOVIES</h1>}/>
+          <Route path='/shows' element={<h1>TV SHOWS</h1>}/>
         </Routes>
       </HashRouter>
     </div>
